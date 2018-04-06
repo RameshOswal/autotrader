8319939ff5d6b9d833673ea1a8be1204eacbfacb
stripathi08 <shubhamt@andrew.cmu.edu>
2018-04-05 20:33:55 -0400

    changing batchifier

--- examples/load_data.py
+++ examples/load_data.py
@@ -14,7 +14,7 @@ class batchify:
         """Divide the prices by the closing price to get Price Relative Vector"""
         return X.divide(X_c.values)
 
-    def load_train(self, bsz = 16, bptt = 50, asset_list = ASSET_LIST):
+    def load_train(self, bsz = 16, bptt = 50, asset_list = ASSET_LIST, normalize = False):
         """
         :param bsz: Batch size
         :param bptt: History
@@ -34,23 +34,32 @@ class batchify:
                 # s_ids are the Batch IDs for this batch, of the form [  0  50 100 150 200 250 300 350 400 450 500 550 600 650 700 750] if bptt == 50
                 s_ids = shuffle_ids[idx * bsz: (idx + 1) * bsz]
 
-                # X.shape => (bptt X num_features X num_assets)
-                X = np.zeros(shape=(bptt, 3, len(asset_list)))
+                # X.shape => (bptt X num_features X num_assets), y.shape => (bptt X num_assets)
+                X = np.zeros(shape = (bptt, 3, len(asset_list)))
+                y = np.zeros(shape = (bptt, len(asset_list)))
 
-                # X.shape => (bsz X bptt X num_features X num_assets)
+                # X.shape => (bsz X bptt X num_assets), y.shape => (bsz X bptt X num_assets)
                 X = X[np.newaxis, :]
+                y = y[np.newaxis, :]
 
                 for s_idx in s_ids:
-                    h_batch = self.create_batches(high.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
-                    l_batch = self.create_batches(low.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
-                    c_batch = self.create_batches(close.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
+                    if normalize:
+                        h_batch = self.create_batches(high.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
+                        l_batch = self.create_batches(low.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
+                        c_batch = self.create_batches(close.iloc[s_idx: s_idx + bptt, :], close.iloc[s_idx + bptt - 1, :])
+                    else:
+                        h_batch = high.iloc[s_idx: s_idx + bptt, :]
+                        l_batch = low.iloc[s_idx: s_idx + bptt, :]
+                        c_batch = close.iloc[s_idx: s_idx + bptt, :]
+
 
                     # Convert all three to matrix form, out.shape => (num_features X bptt X num_assets)
                     out = np.array([h_batch.as_matrix(), l_batch.as_matrix(), c_batch.as_matrix()])
 
                     X = np.vstack((X, np.reshape(out.transpose([1, 0, 2]), [1, bptt, 3, len(asset_list)])))
+                    y = np.vstack((y, np.reshape(close.iloc[s_idx + 1: s_idx + bptt + 1, :].as_matrix(), [1, bptt, len(asset_list)])))
 
-                yield X[1:, :, :, :]
+                return X[1:, :, :, :], y[1:, :, :]
 
     def load_test(self, bsz = 100, bptt = 50, asset_list = ASSET_LIST):
         for vals in zip(self.loader("high", asset_list = asset_list), self.loader("low", asset_list = asset_list), self.loader("close", asset_list = asset_list)):
