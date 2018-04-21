995f0d7e72e564c1fab819f1abd2b7dfac6bb244
saatviks <saatviks@andrew.cmu.edu>
2018-04-20 23:43:47 -0400

    ARIMA mods

--- examples/lab_arima.py
+++ examples/lab_arima.py
@@ -2,16 +2,26 @@ from data_load.load_crypto import DataPreprocess
 from models.arima_model import ARIMAModel
 from get_metrics import get_metrics
 from literals import ASSET_LIST
+import numpy as np
+
+NUM_IDXS = 4
+BPTT = 50
+INIT_PV=1000
+START_PT = 50
+
 
 if __name__=='__main__':
-    dp = DataPreprocess()
-    for train_iter, test_iter, train_date, test_date in dp.load_train_test(asset_name=ASSET_LIST,feature_type='close'):
-        # print(train_date, test_date)
-        ts_data = 1000 * train_iter.values
-        model = ARIMAModel(1, 1, 0, -1, 50)
-        ytrue, ypred = model.evaluate(ts_data[:100], 50)
+    dp = DataPreprocess(input_folder_name="../dataset/Poloneix_Preprocessednew")
+    for idx in range(NUM_IDXS):
+        train_df, test_df = dp.load_train_test(idx=idx,
+                                                   path="../dataset/Poloneix_Preprocessednew",
+                                                   asset_name=ASSET_LIST,feature_type='close')
+        test_data = 1000*np.concatenate((train_df.values[-50:,:], test_df.values))
+        test_date = "_".join(dp.test_dates[idx])
+        model = ARIMAModel(1, 1, 0, -1, BPTT)
+        ytrue, ypred = model.evaluate(test_data, START_PT)
         weights = model.compute_allocation_weights(ypred, ytrue)
         assert (len(ytrue) == len(ypred))
         true_change = ytrue[1:]/ytrue[:-1]
         m = get_metrics(dt_range = test_date)
-        m.apv_multiple_asset(true_change, weights, get_graph=True)
+        m.apv_multiple_asset(true_change, weights, get_graph=True, pv_0=INIT_PV)
