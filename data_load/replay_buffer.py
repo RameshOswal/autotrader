2424e2041b3dedb2319f301403d3dee8f664f222
saatviks <saatviks@andrew.cmu.edu>
2018-05-06 12:44:58 -0400

    Current Replay Buffer

--- data_load/replay_buffer.py
+++ data_load/replay_buffer.py
@@ -2,32 +2,29 @@ import numpy as np
 from collections import deque
 
 class ReplayBuffer:
-    def __init__(self, buffer_size):
+    def __init__(self, buffer_size, rewards=False):
         self.max_size = buffer_size
+        self.rewards = rewards
         # Pool is a list of tuples (states, reward, action)
         self.pool = deque()
         self.size = 0
-        self.num_elem = 0
+        self.ids = np.arange(start=0, stop=self.max_size)
 
-    def add(self, vars, bsz):
+
+    def add(self, state=None, reward=None, action=None):
         """
-        :param vars: list of elements whose first dimensions are EQUAL. [states, rewards, actions, ...]
-            :param state (X): bsz X bptt X num_feats * num_assets
-            :param reward (y): bsz
-            :param action (weights): bsz X num_assets
+        :param state (X): bsz X bptt X num_feats * num_assets
+        :param reward (y): bsz
+        :param action (weights): bsz X num_assets
         """
         # assert len(state) == len(reward) and len(reward) == len(action), "Dimension Mismatch (REPLAY BUFFER)"
-        self.num_elem = len(vars)
 
-        for batch in range(bsz):
-            if self.size < self.max_size:
-                self.pool.append(tuple([elem[batch] for elem in vars]))
-                self.size += 1
-            else:
-                self.pool.popleft()
-                self.pool.append(tuple([elem[batch] for elem in vars]))
-
-        assert len(self.pool) == self.size, "Error in Adding Elements to Buffer! Sizes = {} and {}".format(len(self.pool), self.size)
+        if self.size < self.max_size:
+            self.pool.append((state, reward, action))
+            self.size += 1
+        else:
+            self.pool.popleft()
+            self.pool.append((state, reward, action))
 
     def clear(self):
         self.pool = deque()
@@ -38,20 +35,13 @@ class ReplayBuffer:
         :param bsz: batch size
         :return: list of vars [states, rewards, actions, ...]
         """
-        ids = np.arange(start=0, stop=self.size)
-        np.random.shuffle(ids)
+        np.random.shuffle(self.ids)
+        elems = [self.pool[x] for x in self.ids[:bsz]]
 
-        out_vars = {_ : [] for _ in range(self.num_elem)}
-        for batch in ids[:bsz]:
-            tup = self.pool[batch]
-            for idx in range(len(tup)):
-                out_vars[idx].append(tup[idx])
+        # print(elems[0])
+        states = np.concatenate([x[0] for x in elems], axis=0)
+        rewards = None if self.rewards == False else np.concatenate([x[1] for x in elems], axis=0)
+        actions = np.concatenate([x[2] for x in elems], axis=0)
 
-        # Pop out the elements
-        for _ in range(bsz): self.pool.popleft()
-
-        self.size -= bsz
-
-        assert len(self.pool) == self.size, "Error in Replay Buffer Pool Size!"
-
-        return [np.stack(out_vars[x], axis = 0) for x in out_vars]
+        # print(states.shape, rewards, actions.shape)
+        return states, rewards, actions
